2cdcf3f4114a8d15c58b050de3cf7f0344de5c73
wind on sun <aawind@yandex.ru>
2012-09-15 04:13:26 +0300

    global refactoring

--- dynamictables/static/js/dynamictables.js
+++ dynamictables/static/js/dynamictables.js
@@ -5,190 +5,355 @@
 
 //{% load dajaxice_templatetags %}
 
+function DynamicTablesConsts() {
+    this.COL_ID_NUM = 1;
+    this.COL_TITLE_NUM = 0;
+    this.COL_TYPE_NUM = 2;
+
+    this.CELL_VALUE_NUM = 1;
+    this.CELL_ID_NUM = 0;
+
+    this.ROW_ID_NUM = 0;
+    this.ROW_DATA_NUM = 1;
+
+    this.VALUE_ONCLICK = "onclick='dynamicTables.enable_edit($(this));'";
+}
+
 function DynamicTables() {
     var th = this;
-    var value_onclick = "onclick='dynamicTables.enable_edit($(this));'";
-    var temp = [];
-    var last_editor = new Object;
-    var valuer = new Object;
-    valuer.queue = {};
-    valuer.id = 0;
-    var table_id_list = [];
-    
-    var COL_ID_NUM = 1;
-    var COL_TITLE_NUM = 0;
-    var COL_TYPE_NUM = 2;
-    
-    var CELL_VALUE_NUM = 1;
-    var CELL_ID_NUM = 0;
-    
-    var ROW_ID_NUM = 0;
-    var ROW_DATA_NUM = 1;
-    
-    function fillTable(data) {
+    this.table_id_list = [];
+
+    var tableDrawer = new TableDrawer();
+    var editor = new Editor();
+    var cell = new Cell();
+    cell.editor = editor;
+    cell.temp = tableDrawer.temp;
+
+    function fillTableFromAjax(data) {
+        tableDrawer.data = data;
+        tableDrawer.tablePanel = th.tablePanel;
+        tableDrawer.draw();
+    }
+    this.validate_editor = function() {
+        editor.validator.validate();
+    }
+    this.enable_edit = function($cell) {
+        cell.$cell = cell;
+        cell.enable_edit();
+    }
+    this.appendRow = function() {
+        tableDrawer.rowsEditor.appendRow();
+    }
+    this.removeRow = function(row_id) {
+        tableDrawer.rowsEditor.removeRow();
+    }
+    this.pushTableId = function(id) {
+        th.table_id_list.push(id);
+    }
+    this.activateFirstTable = function() {
         try {
-            th.tablePanel.innerHTML = "<table>" + getHeadHtml(data) +
-                getRowsHtml(data) + "</table>";
+            th.activateTableById(th.table_id_list[0]);
+        } catch(e) {}
+    }
+    this.activateTableById = function(id) {
+        Dajaxice.dynamictables.get_table(fillTableFromAjax, {'table':id});
+    }
+    this.activateTable = function($table) {
+        th.activateTableById($table.attr('id'));
+    }
+}
+
+function TableDrawer() {
+    var th = this;
+    th.rowsEditor = new RowsEditor();
+    th.colsBuffer = new ColsBuffer();
+    var headDrawer =  new HeadDrawer(th.colsBuffer);
+    var rowsDrawer = new RowsDrawer(th.colsBuffer);
+
+    function draw() {
+        try {
+            headDrawer.data = th.data;
+            rowsDrawer.data = th.data;
+            th.tablePanel.innerHTML = "<table>" + headDrawer.draw() +
+                rowsDrawer.draw() + "</table>";
         } catch (e) {}
     }
-    function getHeadHtml(data) {
-        var head = "";
-        temp.length = 0;
-        
-        function fillCell(cellData) {
-            head += "<td id='" + cellData.id + "'>" + cellData.title + "</td>";
-            temp.push(cellData);
-        }
-        function CellData(cellArray) {
-            this.id = cellArray[COL_ID_NUM];
-            this.title = cellArray[COL_TITLE_NUM];
-            this.type = cellArray[COL_TYPE_NUM];
-        }
-        
-        for (var i=0; i < data.columns.length; ++i) {
-            var cellData = new CellData(data.columns[i]);
+}
+
+function HeadDrawer(colsBuffer) {
+    var th = this;
+    th.colsBuffer = colsBuffer;
+    var head = "";
+
+    function draw() {
+        head = "";
+        th.colsBuffer.clear();
+        for (var i=0; i < th.data.columns.length; ++i) {
+            var cellData = new CellData(th.data.columns[i]);
             fillCell(cellData);
         }
         return "<tr>"+head+"</tr>";
-	}
-    function clearTempValues() {
-        for (var j=0; j < temp.length; ++j) {
-            temp[j].value = '';
-        }
     }
-    function generateRowInner(row_id) {
-        var rowHtml = "<tr id='" + row_id+ "'>";
-        for (var j=0; j < temp.length; ++j) {
-            rowHtml += "<td id='" + temp[j].id +
-                "' " + value_onclick + ">" + temp[j].value + "</td>";
-        }
-        rowHtml += "<td><a href='#' onclick='dynamicTables.removeRow("+row_id+");'>Del row...</a></td>";
-        return rowHtml + "</tr>";
+    function fillCell(cellData) {
+        head += "<td id='" + cellData.id + "'>" + cellData.title + "</td>";
+        colsBuffer.push(cellData);
     }
-    function getClearRowInner(row_id) {
-        clearTempValues();
-        return generateRowInner(row_id);
+    function CellData(cellArray) {
+        this.id = cellArray[DynamicTablesConsts.COL_ID_NUM];
+        this.title = cellArray[DynamicTablesConsts.COL_TITLE_NUM];
+        this.type = cellArray[DynamicTablesConsts.COL_TYPE_NUM];
     }
-    function getRowsHtml(data) {
+}
+
+function RowsDrawer(colsBuffer) {
+    var th = this;
+    var rowDrawer = new RowDrawer(colsBuffer);
+
+    this.draw = function() {
         var allRowsHtml = "";
-
-        function fillTempValues(rowData) {
-            for (var j=0; j < rowData.length; ++j) {
-                var id = rowData[j][CELL_ID_NUM];
-                for (var k=0; k < temp.length; ++k) {
-                    if (temp[k].id == id) {
-                        temp[k].value = rowData[j][CELL_VALUE_NUM];
-                        break;
-                    }
-                }
-            }
-        }
-        function getTempSumToRow(row) {
-            var row_id = row[ROW_ID_NUM];
-            var rowHtml = "<tr id='" + row_id+ "'>";
-            rowHtml += generateRowInner(row_id);
-            return rowHtml + "</tr>";
-        }
-        function getRowHtml(row) {
-            clearTempValues();
-            fillTempValues(row[ROW_DATA_NUM]);
-            return getTempSumToRow(row);
-        }
-
-        for (var i=0; i < data.rows.length; ++i) {
-            allRowsHtml += getRowHtml(data.rows[i]);
+        for (var i=0; i < th.data.rows.length; ++i) {
+            allRowsHtml += rowDrawer.draw(th.data.rows[i]);
+                //getRowHtml(th.data.rows[i]);
         }
         allRowsHtml += "<tr><td><a href='#' onclick='dynamicTables.appendRow();'>Add row...</a></td></tr>";
         return allRowsHtml;
     }
+}
+
+function RowDrawer(colsBuffer) {
+    this.draw = function(row) {
+        colsBuffer.clearTempValues();
+        colsBuffer.fillTempValues(row[DynamicTablesConsts.ROW_DATA_NUM]);
+        return getTempSumToRow(row);
+    }
+    function getTempSumToRow(row) {
+        var row_id = row[DynamicTablesConsts.ROW_ID_NUM];
+        var rowHtml = "<tr id='" + row_id+ "'>";
+        rowHtml += generateRowInner(row_id);
+        return rowHtml + "</tr>";
+    }
+    function generateRowInner(row_id) {
+        var rowHtml = "<tr id='" + row_id+ "'>";
+        rowHtml += colsBuffer.toHtml();
+        rowHtml += "<td><a href='#' onclick='dynamicTables.removeRow("+row_id+");'>Del row...</a></td>";
+        return rowHtml + "</tr>";
+    }
+    this.getClearRowInner = function(row_id) {
+        colsBuffer.clearTempValues();
+        return generateRowInner(row_id);
+    }
+}
+
+function ColsBuffer() {
+    var th = this;
+    th.temp = [];
+
+    th.clear = function() {
+        th.temp.length = 0;
+    }
+    th.push = function(cellData) {
+        th.temp.push(cellData);
+    }
+    th.clearTempValues = function() {
+        for (var j=0; j < th.temp.length; ++j) {
+            th.temp[j].value = '';
+        }
+    }
+    th.fillTempValues = function(rowData) {
+        for (var j=0; j < rowData.length; ++j) {
+            var id = rowData[j][DynamicTablesConsts.CELL_ID_NUM];
+            for (var k=0; k < th.temp.length; ++k) {
+                if (th.temp[k].id == id) {
+                    th.temp[k].value = rowData[j][DynamicTablesConsts.CELL_VALUE_NUM];
+                    break;
+                }
+            }
+        }
+    }
+    th.toHtml = function() {
+        var html = '';
+        for (var j=0; j < th.temp.length; ++j) {
+            html += "<td id='" + th.temp[j].id +
+                "' " + DynamicTablesConsts.VALUE_ONCLICK + ">"
+                + th.temp[j].value + "</td>";
+        }
+        return html;
+    }
+}
+
+function EditorValidator(mainEditor) {
+    var th = this;
+    th.valuer = new Object;
+    th.valuer.queue = {};
+    th.valuer.id = 0;
+
+    th.validate = function() {
+        var editor = mainEditor.getEditorByType(th.editor.type);
+        editor.validator.validate_edit();
+        clear_last_editor();
+    }
+    function clear_last_editor() {
+        th.editor.type = false;
+        th.editor.$cell = false;
+        th.editor.$instance = false;
+    }
+    function doValidate(val, f_type, $cell) {
+        th.valuer.queue[th.valuer.id]=$cell;
+        var f = {'field':val+''};
+        Dajaxice.dynamictables.send_form(updateCell, {'form':f,
+            'f_type':f_type, 'id':th.valuer.id, 'col':$cell.attr('id'), 'row':$cell.parent().attr('id')});
+        ++(th.valuer.id);
+    }
+    function updateCell(data) {
+        var $cell = th.valuer.queue[data.id];
+        var color = "#009900";
+        if (data.errors.length) {
+            color = "#ff0000";
+        }
+        $cell.css("border", "1px solid "+color);
+    }
+}
+
+function SimpleEditorValidator(mainValidator) {
+    var th = this;
+
+    th.validate_edit = function() {
+        var val = th.editor.$instance.value;
+        th.editor.$cell.text(val);
+        mainValidator.doValidate(val, th.editor.type, th.editor.$cell);
+    }
+}
+
+function DateEditorValidator(mainValidator) {
+    var th = this;
+
+    th.validate_edit = function() {
+        var $dph = $('#datepicker_holder');
+        th.editor.$instance.detach().prependTo($dph);
+        var dt = th.editor.$instance.datepicker('getDate');
+        var dt_s = $.datepicker.formatDate(
+            'yy-mm-dd',
+            th.editor.$instance.datepicker("getDate")
+        );
+        th.editor.$cell.text(dt_s);
+        mainValidator.doValidate(dt_s, 'D', th.editor.$cell);
+    }
+}
+
+function Editor() {
+    var th = this;
+    th.validator = new EditorValidator(this);
+    var simpleEditor = new SimpleEditor(th.validator);
+    var dateEditor = new DateEditor(th.validator);
+
+    th.enableEditorInCell = function($cell, col_type) {
+        th.editor.value = $cell.text();
+        th.editor.type = col_type;
+        var type_html = getTypeHtml(th.editor.type);
+        var editor = getEditorByType(th.editor.type);
+        editor.enableEditor($cell, type_html);
+    }
+    function getEditorByType(type) {
+        switch (type) {
+            case 'C':
+            case 'I':
+                return simpleEditor;
+            case 'D':
+                return dateEditor;
+        }
+        return false;
+    }
+    function getTypeHtml(type) {
+        if (type == 'C') {
+            return 'text';
+        } else if (type == 'I') {
+            return 'int';
+        }
+        return '';
+    }
+}
+function SimpleEditor(mainValidator) {
+    var th = this;
+    th.validator = new SimpleEditorValidator(mainValidator);
+    th.validator.editor = this;
+
+    th.enableEditor = function($cell, type) {
+        $cell.html("<input type='"+type+"' value='"+$cell.text()+"' onblur='dynamicTables.validate_editor();' />");
+        th.editor.$cell = $cell;
+        th.editor.$instance = $cell.children()[0];
+        th.editor.$instance.focus();
+    }
+}
+function DateEditor(mainValidator) {
+    var th = this;
+    th.validator = new DateEditorValidator(mainValidator);
+    th.validator.editor = this;
+
+    th.enableEditor = function($cell) {
+        var $dp = $('#datepicker_holder>input');
+        $dp.val(th.editor.value);
+
+        $dp.datepicker( "setDate" , $cell.text() );
+        $cell.text('');
+
+        $dp.detach().prependTo($cell);
+        $dp.focus();
+        th.editor.$instance = $dp;
+        th.editor.$cell = $cell;
+    }
+}
+
+function Cell() {
+    var th = this;
+
+    this.enable_edit = function() {
+        var row = th.$cell.parent().attr('id');
+        var col = th.$cell.attr('id');
+        var col_type = getColumnType(col);
+
+        if (isOnInput()) {
+            return;
+        }
+        th.editor.enableEditorInCell(th.$cell, col_type);
+    }
     function getColumnType(columnId) {
-        for (var i=0; i<temp.length; ++i) {
-            if (temp[i].id==columnId) {
-              return temp[i].type;
+        for (var i=0; i<th.temp.length; ++i) {
+            if (th.temp[i].id==columnId) {
+                return th.temp[i].type;
             }
         }
         return false;
     }
-    this.validate_editor = function() {
-        switch (last_editor.type) {
-        case 'C':
-        case 'I':
-            var val = last_editor.$instance.value;
-            last_editor.$cell.text(val);
-            validate(val, last_editor.type, last_editor.$cell);
-            break;
-        case 'D': 
-            var $dph = $('#datepicker_holder');
-            last_editor.$instance.detach().prependTo($dph);
-            dt = last_editor.$instance.datepicker('getDate');
-            dt_s = $.datepicker.formatDate(
-                'yy-mm-dd',
-                last_editor.$instance.datepicker("getDate")
-            );
-            last_editor.$cell.text(dt_s);
-            validate(dt_s,'D',last_editor.$cell);
-            break;
-        }
-        last_editor.type = false;
-        last_editor.$cell = false;
-        last_editor.$instance = false;
-    }
-    function isOnInput($cell) {
+    function isOnInput() {
         var tg = false;
         try {
-            tg = $cell.children()[0].tagName;
+            tg = th.$cell.children()[0].tagName;
         } catch (e) {}
         if (tg == "input" || tg == "INPUT" ) {
             return true;
         }
         return false;
     }
-    this.enable_edit = function($cell) {
-        var row = $cell.parent().attr('id');
-        var col = $cell.attr('id');
-        var col_type = getColumnType(col);
-        
-        if (isOnInput($cell)) {
-            return;
-        }
+}
 
-        last_editor.value = $cell.text();
-      
-        last_editor.type = col_type;
-        
-        function enableSimpleEditor($cell, type) {
-            $cell.html("<input type='"+type+"' value='"+$cell.text()+"' onblur='dynamicTables.validate_editor();' />");
-            last_editor.$cell = $cell;
-            last_editor.$instance = $cell.children()[0];
-            last_editor.$instance.focus();
-        }
-        function enableDateEditor($cell) {
-            var $dp = $('#datepicker_holder>input');
-            $dp.val(last_editor.value);
-
-            $dp.datepicker( "setDate" , $cell.text() );
-            $cell.text('');
-
-            $dp.detach().prependTo($cell);
-            $dp.focus();
-            last_editor.$instance = $dp;
-            last_editor.$cell = $cell;
-        }
-        
-        switch (col_type) {
-        case 'C':
-            enableSimpleEditor($cell, 'text');
-            break;
-        case 'I':
-            enableSimpleEditor($cell, 'int');
-            break;
-        case 'D':
-            enableDateEditor($cell);
-            break;
-        }
-      
+function RowsEditor() {
+    var th = this;
+    var rowDrawer = new RowDrawer();
+    
+    this.appendRow = function() {
+        var newRow = document.createElement('tr');
+        newRow.innerHTML = rowDrawer.getClearRowInner(-1);
+        newRow.setAttribute("id", '-1');
+        var table = th.tablePanel.firstChild;
+        table.insertBefore(newRow, table.lastChild);
+        //table.appendChild(newRow);
     }
-
+    this.removeRow = function(row_id) {
+        //try {
+        var row = getRowById(row_id);
+        row.parentNode.removeChild(row);
+        //} catch (e) {}
+    }
     function getRowById(row_id) {
         try {
             var table = th.tablePanel.firstChild();
@@ -201,55 +366,8 @@ function DynamicTables() {
         } catch (e) {}
         return false;
     }
-
-    this.appendRow = function() {
-        var newRow = document.createElement('tr');
-        newRow.innerHTML = getClearRowInner(-1);
-        newRow.setAttribute("id", '-1');
-        var table = th.tablePanel.firstChild;
-        table.insertBefore(newRow, table.lastChild);
-        //table.appendChild(newRow);
-    }
-    this.removeRow = function(row_id) {
-        //try {
-            var row = getRowById(row_id);
-            row.parentNode.removeChild(row);
-        //} catch (e) {}
-    }
-
-
-    function validate(val, f_type, $cell) {
-        valuer.queue[valuer.id]=$cell;
-        var f = {'field':val+''};
-        Dajaxice.dynamictables.send_form(updateCell, {'form':f,
-            'f_type':f_type, 'id':valuer.id, 'col':$cell.attr('id'), 'row':$cell.parent().attr('id')});
-        ++(valuer.id);
-    }
-
-    function updateCell(data) {
-        var $cell = valuer.queue[data.id];
-        var color = "#009900";
-        if (data.errors.length) {
-            color = "#ff0000";
-        }
-        $cell.css("border", "1px solid "+color);
-    }
-
-    this.pushTableId = function(id) {
-        table_id_list.push(id);
-    }
-    this.activateFirstTable = function() {
-        try {
-            th.activateTableById(table_id_list[0]);
-        } catch(e) {}
-    }
-    this.activateTableById = function(id) {
-        Dajaxice.dynamictables.get_table(fillTable, {'table':id});
-    }
-    this.activateTable = function($table) {
-        th.activateTableById($table.attr('id'));
-    }
 }
+
 
 //{% dajaxice_js_import %}
 
