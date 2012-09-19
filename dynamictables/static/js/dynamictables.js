8fc133c19b65686923f3a53663b5fb947935b015
Andrey on ahome <aa.veter@gmail.com>
2012-09-20 02:00:19 +0300

    -rowData

--- dynamictables/static/js/dynamictables.js
+++ dynamictables/static/js/dynamictables.js
@@ -139,6 +139,7 @@ function RowsDrawer(colsBuffer) {
             var tbody = getTableBody();
             for (var i=0; i<tbody.childNodes.length; ++i) {
                 var row = tbody.childNodes[i];
+                alert(row.getAttribute('id'));
                 if (row.getAttribute('id')==row_id) {
                     return row;
                 }
