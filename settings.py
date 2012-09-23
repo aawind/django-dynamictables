6194f74cb277aae6a964f3bb74b33128278aecda
wind on sun <aawind@yandex.ru>
2012-09-23 17:06:59 +0300

    jenkins

--- settings.py
+++ settings.py
@@ -68,3 +68,11 @@ INSTALLED_APPS = (
     'dynamictables',
     'south',
 )
+
+#jenkins
+INSTALLED_APPS += ('django_jenkins',)
+JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+                 'django_jenkins.tasks.run_pep8',
+                 'django_jenkins.tasks.run_pyflakes',
+                 'django_jenkins.tasks.with_coverage',
+                 'django_jenkins.tasks.django_tests',)
